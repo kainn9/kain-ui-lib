9dc8de773fa9ebd17600749d6b3b251156d03ab7
kainn9 <kaindoneson@gmail.com>
2023-03-12 20:22:52 -0400

    fix: removed inherit height from hoc(added note) and using vh for pageLoader

--- src/hoc/uiWrapper.tsx
+++ src/hoc/uiWrapper.tsx
@@ -2,17 +2,16 @@ import { type ComponentType } from "react"
 
 const wrapperPrefix = "kain-ui-lib"
 
+/*
+* Note: If the child component needs height: 100%
+* this HOC will break it. The class name should be applied
+* to the highest level element of the component instead.
+*/
 export function uiWrapper<T extends ComponentType<any>> (Component: T) {
   // eslint-disable-next-line react/display-name
   return (props: React.ComponentProps<T>) => {
     return (
-      <div
-        style={{
-          height: "inherit",
-          width: "inherit"
-        }}
-        className={wrapperPrefix}
-      >
+      <div className={wrapperPrefix}>
         <Component {...props} />
       </div>
     )
